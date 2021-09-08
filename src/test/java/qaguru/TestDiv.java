30b78e539ca98dec06b839890eeb28c3237d40f0
Bolgar <bolgar888@gmail.com>
2021-09-08 20:36:34 +0300

    Тест для страницы githubWiki

---
+++ src/test/java/qaguru/TestDiv.java
@@ -0,0 +1,20 @@
+package qaguru;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+
+class Practice {
+    @Test
+    void TestDragAndDrop() {
+        open("http://uitestingplayground.com/");
+        // Первое значение отработает
+        $("div h3").$(byText("Dynamic ID")).click();
+        // Второе падает с ошибкой Element not found {div/h3}
+        $("div").$("h3").$(byText("Dynamic ID")).click();
+
+    }
+}
---
+++ src/test/java/qaguru/draganddrop.java
@@ -0,0 +1,18 @@
+package qaguru;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+
+public class draganddrop {
+    @Test
+    void TestDragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+         $("#column-a").dragAndDropTo("#column-b");
+         $("#column-a").shouldHave(text("B"));
+         $("#column-b").shouldHave(text("A"));
+    }
+}

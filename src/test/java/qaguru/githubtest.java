50d178606bb954ab5bb6b5c2d76e01adbe2c3ed9
Bolgar <bolgar888@gmail.com>
2021-09-08 16:07:00 +0300

    Тест для страницы githubWiki

---
+++ src/test/java/qaguru/githubtest.java
@@ -0,0 +1,25 @@
+package qaguru;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+
+public class githubtest {
+    @Test
+    void GithubTestWiki() {
+        // открыть страничку гитхаб
+        open("https://github.com/selenide/selenide");
+        // перейти на вики
+        $("#wiki-tab").click();
+        // Перейти в Soft assertions
+        $("#wiki-body").$(byText("Soft assertions")).click();
+        // Поиск Junit
+        $(".gollum-markdown-content").shouldHave(text("JUnit5 extension - "));
+
+    }
+
+}
+

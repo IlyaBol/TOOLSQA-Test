f3c64edbe5e84ef7e9a3d8b3a3449d9fe92bfa87
Bolgar <bolgar888@gmail.com>
2021-09-15 22:22:34 +0300

    1. Задание разница между $("h1 div"); и $("h1").$("div")

--- src/test/java/qaguru/TestDiv.java
+++ src/test/java/qaguru/TestDiv.java
@@ -1,17 +1,15 @@
 package qaguru;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestDiv {
     @Test
     void verificationDivAndH() {
+
         open("http://uitestingplayground.com/");
         // Первое значение отработает
         $("div h3").$(byText("Dynamic ID")).click();

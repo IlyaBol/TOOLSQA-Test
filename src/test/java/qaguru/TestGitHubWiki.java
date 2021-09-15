d87445bed4aa26391f9cacf344567b8c5401de93
Bolgar <bolgar888@gmail.com>
2021-09-15 22:19:12 +0300

    2.Задание поиск на Wiki

--- src/test/java/qaguru/TestGitHubWiki.java
+++ src/test/java/qaguru/TestGitHubWiki.java
@@ -1,13 +1,11 @@
 package qaguru;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestGitHubWiki {
     @Test
